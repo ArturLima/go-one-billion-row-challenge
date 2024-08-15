a41f4132d34b2fcac59699ec73dc8c7d29cda040
Artur Lima <arturpeixotoo@gmail.com>
2024-08-15 20:58:57 -0300

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ArturLima/go-one-billion-row-challenge
+
+go 1.23
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Measurement struct {
+	Min   float64
+	Max   float64
+	Sum   float64
+	Count int64
+}
+
+func main() {
+	measurements, err := os.Open("measurements.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer measurements.Close()
+
+	scanner := bufio.NewScanner(measurements)
+
+	dados := make(map[string]Measurement)
+
+	for scanner.Scan() {
+		rawData := scanner.Text()
+		semiColon := strings.Index(rawData, ";")
+		location := rawData[:semiColon]
+		rawTemp := rawData[semiColon+1:]
+
+		temp, _ := strconv.ParseFloat(rawTemp, 64)
+
+		measurement, ok := dados[location]
+		if !ok {
+			measurement = Measurement{
+				Min:   temp,
+				Max:   temp,
+				Sum:   temp,
+				Count: 1,
+			}
+		} else {
+			measurement.Min = min(measurement.Min, temp)
+			measurement.Max = max(measurement.Max, temp)
+			measurement.Sum += temp
+			measurement.Count++
+
+		}
+		dados[location] = measurement
+	}
+
+	locations := make([]string, 0, len(dados))
+
+	for name := range dados {
+		locations = append(locations, name)
+	}
+
+	sort.Strings(locations)
+
+	for _, name := range locations {
+		measurement := dados[name]
+		fmt.Printf(
+			"%s=%.1f/%.1f/%.1f, ",
+			name,
+			measurement.Min,
+			measurement.Sum/float64(measurement.Count),
+			measurement.Max,
+		)
+	}
+	fmt.Printf("}\n")
+}
